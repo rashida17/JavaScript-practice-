bb515cdd0576c61a978172f2dfa74e7a0ab2a63e
Rashida Iqbal <bscs824@gmail.com>
2026-03-05 09:31:01 +0000

    variable done

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const rollNo =115311;
+let name = "Rashida";
+var age=21;
+let graduate;
+let married=false
+
+console.table([rollNo,name,age,graduate]);
+console.log( typeof rollNo)
+console.log( typeof name)
+console.log( typeof graduate)
+console.log(typeof married)
